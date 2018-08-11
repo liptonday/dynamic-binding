1d0e2c6adb77b14c5f41386fda4787f44d955d29
TaoTR <liptonday71@gmail.com>
2018-08-11 18:16:33 -0400

    Initial Commit

---
+++ DynamicBinding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicBinding
+//
+//  Created by ZHITAO TIAN on 2018-08-11.
+//  Copyright © 2018 ZHITAO TIAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
